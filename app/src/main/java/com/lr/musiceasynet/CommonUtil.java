8e13b9a223ff2b6f714cd0272890d37d838963ce
lightningrealm <40555036+lightningrealm@users.noreply.github.com>
2024-04-05 18:59:36 +0800

    deleteRepeatResource

--- app/src/main/java/com/lr/musiceasynet/CommonUtil.java
+++
@@ -1,9 +0,0 @@
-package com.lr.musiceasynet;
-
-public class CommonUtil {
-
-    public static boolean isDarkMode(){
-        return MyApplication.getContext().getResources().getConfiguration().uiMode == 0x21;
-    }
-
-}
