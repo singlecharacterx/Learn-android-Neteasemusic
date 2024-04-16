21609c449f651835b828b42df659ead0d1b9d486
lightningrealm <40555036+lightningrealm@users.noreply.github.com>
2024-04-16 19:44:10 +0800

    Create CommonUtil.java

---
+++ app/src/main/java/com/lr/musiceasynet/util/CommonUtil.java
@@ -0,0 +1,29 @@
+package com.lr.musiceasynet.util;
+
+import android.content.Context;
+import android.view.View;
+
+import androidx.activity.ComponentActivity;
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CommonUtil {
+
+    private CommonUtil(){}
+
+    public static boolean isDarkMode(Context context){
+        return context.getResources().getConfiguration().uiMode == 0x21;
+    }
+
+    public static void edgeToEdge(ComponentActivity componentActivity, View root){
+        EdgeToEdge.enable(componentActivity);
+        ViewCompat.setOnApplyWindowInsetsListener(root, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
+            return insets;
+        });
+    }
+
+}
